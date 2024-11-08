936337cdfbecbddaa228e348fca9f4718aad6398
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2024-11-08 14:26:25 +0330

    initial commit

---
+++ pages/api/contact.js
@@ -0,0 +1,31 @@
+import { supabase } from "@/lib/supabase";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST")
+    return res.status(405).json({
+      success: false,
+      message: "Please make a POST request",
+    });
+
+  // const contactData = {
+  //   fullName: "Somayeh Mokarrami Roatami",
+  //   email: "s.m@gmail.com",
+  //   subject: "create Api Router",
+  //   message: "Hello My Friends",
+  // };
+  const contactData = JSON.parse(req.body);
+
+  const { error } = await supabase.from("contact").insert([contactData]);
+
+  if (error)
+    res.status(500).json({
+      success: false,
+      message: "Could not send your message. Please try again",
+    });
+
+  // Success message
+  res.status(200).json({
+    success: true,
+    message: "Thanks for your message! We will be in touch soon :)",
+  });
+}
